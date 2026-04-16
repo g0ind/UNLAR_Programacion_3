36ad1e1b36a87200b4f59ebb952d79e0d1e270bd
Virginia Vera <virginiavh454@gmail.com>
2026-04-16 11:30:02 -0300

    Complete la entidad estudiante y aparte cree la clase prestamo

--- TP1_Biblioteca/biblioteca/src/main/java/unlar/edu/ar/model/Estudiante.java
+++ TP1_Biblioteca/biblioteca/src/main/java/unlar/edu/ar/model/Estudiante.java
@@ -1,5 +1,61 @@
 package unlar.edu.ar.model;
 
 public class Estudiante {
+    // 2.1 Atributos privados (Encapsulamiento)
+    private String legajo;
+    private String nombre;
+    private String carrera;
+    private String email;
 
+    // 3.1 Constructor parametrizado (para crear el estudiante con datos)
+    public Estudiante(String legajo, String nombre, String carrera, String email) {
+        this.legajo = legajo;
+        this.nombre = nombre;
+        this.carrera = carrera;
+        this.email = email;
+    }
+
+    // 3.1 Constructor por defecto (vacio, por si se necesita)
+    public Estudiante() {
+    }
+
+    // 3.0 Getters y Setters (Las "puertas" de acceso)
+    public String getLegajo() {
+        return legajo;
+    }
+
+    public void setLegajo(String legajo) {
+        this.legajo = legajo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCarrera() {
+        return carrera;
+    }
+
+    public void setCarrera(String carrera) {
+        this.carrera = carrera;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    // 3.2 Método toString() para mostrar los datos lindos en la consola
+    @Override
+    public String toString() {
+        return "Estudiante: [Legajo=" + legajo + ", Nombre=" + nombre + 
+               ", Carrera=" + carrera + ", Email=" + email + "]";
+    }
 }
