f962d9d7527f2f7df023e5dc9dfcb65ec7df47b9
pablo <galarzaph@outlook.com>
2026-04-24 13:55:11 -0300

    Exception Logic

---
+++ atm/src/main/java/unlar/edu/ar/exception/LimiteExtraccionExcedido.java
@@ -0,0 +1,7 @@
+package unlar.edu.ar.exception;
+
+public class LimiteExtraccionExcedidoException extends Exception {
+    public LimiteExtraccionExcedidoException(String mensaje) {
+        super(mensaje);
+    }
+}
---
+++ atm/src/main/java/unlar/edu/ar/exception/SaldoInsuficiente.java
@@ -0,0 +1,7 @@
+package unlar.edu.ar.exception;
+
+public class SaldoInsuficienteException extends Exception {
+    public SaldoInsuficienteException(String mensaje) {
+        super(mensaje);
+    }
+}
