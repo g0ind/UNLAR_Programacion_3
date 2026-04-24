1edb9761eef84cbf442ddf0f12a5a6a8748411a4
pablo <galarzaph@outlook.com>
2026-04-24 13:03:18 -0300

    creacion de clases basicas en /model y /serivce

---
+++ atm/src/model/CuentaBancaria.java
@@ -0,0 +1,33 @@
+package model;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
+public class CuentaBancaria {
+    private final String numeroCuenta; // Inmutable
+    private BigDecimal saldo;
+    private String titular;
+    private boolean activa;
+    private List<Transaccion> historialTransacciones;
+
+    public CuentaBancaria(String numeroCuenta, String titular, BigDecimal saldoInicial) {
+        this.numeroCuenta = numeroCuenta;
+        this.titular = titular;
+        this.saldo = saldoInicial;
+        this.activa = true;
+        this.historialTransacciones = new ArrayList<>();
+    }
+
+    public String getNumeroCuenta() { return numeroCuenta; }
+    public BigDecimal getSaldo() { return saldo; }
+    public void setSaldo(BigDecimal saldo) { this.saldo = saldo; }
+    public String getTitular() { return titular; }
+    public boolean isActiva() { return activa; }
+    public void setActiva(boolean activa) { this.activa = activa; }
+    public List<Transaccion> getHistorialTransacciones() { return historialTransacciones; }
+
+    public void agregarTransaccion(Transaccion t) {
+        this.historialTransacciones.add(t);
+    }
+}
