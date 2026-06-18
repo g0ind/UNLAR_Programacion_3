0f2b70080a739ab149657f660ca53e3eb00f001e
Marisa Chaile <marisasolchaile@gmail.com>
2026-06-17 22:37:59 -0300

    creacion del project tp6

---
+++ TP6_Comparable_Comparator/comparable_comparator/src/test/java/ar/edu/unlar/comparable_comparator/ComparableComparatorApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.edu.unlar.comparable_comparator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComparableComparatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
